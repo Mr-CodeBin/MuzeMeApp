4aa596f72dcdc4982ce862e2215da727bcfc5255
Nikhil Singh <nikhilsingh12032@gmail.com>
2023-03-21 02:11:41 +0530

    made changes in libpage Layout

--- lib/pages/librarypage.dart
+++ lib/pages/librarypage.dart
@@ -36,20 +36,107 @@ class _libPageState extends State<libPage> {
                 ),
               ),
             ),
-            CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  expandedHeight: 500,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: MusicCategory(),
+            Container(
+              margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
+              padding: EdgeInsets.all(12),
+              width: size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.black,
+              ),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(1000),
+                    child: Container(
+                      height: 160,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        // borderRadius: BorderRadius.circular(12),
+                        color: Colors.blue,
+                      ),
+                      child: Image.network(
+                        "https://images.pexels.com/photos/2742812/pexels-photo-2742812.jpeg?auto=compress&cs=tinysrgb&w=800",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-                SliverToBoxAdapter(
-                  child: Text(
-                      """ RenderBox was not laid out: RenderRepaintBoundary#f2b6b relayoutBoundary=up1 NEEDS-PAINT'package:flutter/src/rendering/box.dart':box.dart:1 RenderBox was not laid out: RenderRepaintBoundary#f2b6b relayoutBoundary=up1 NEEDS-PAINT'package:flutter/src/rendering/box.dart':box.dart:1 RenderBox was not laid out: RenderRepaintBoundary#f2b6b relayoutBoundary=up1 NEEDS-PAINT'package:flutter/src/rendering/box.dart':box.dart:1 RenderBox was not laid out: RenderRepaintBoundary#f2b6b relayoutBoundary=up1 NEEDS-PAINT'package:flutter/src/rendering/box.dart':box.dart:1 """),
-                )
-              ],
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      // borderRadius: BorderRadius.circular(12),
+                      color: Colors.transparent,
+                    ),
+                    child: Text(
+                      "Constant"
+                      "\nConnected By",
+                      style: GoogleFonts.overpass(
+                          fontSize: 18,
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  )
+                ],
+              ),
             ),
+            Container(
+              height: 300,
+              child: GridView.count(
+                childAspectRatio: 2,
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                padding: EdgeInsets.all(12),
+                children: [
+                  Container(
+                    child: Text("My Playlist"),
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black,
+                    ),
+                  ),
+                  Container(
+                    child: Text("Albums"),
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black,
+                    ),
+                  ),
+                  Container(
+                    child: Text("Liked Songs"),
+                    height: 20.0,
+                    width: 20.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black,
+                    ),
+                  ),
+                  Container(
+                    child: Text("Favourite Artists"),
+                    height: 20.0,
+                    width: 20.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              width: size.width,
+              color: Colors.black,
+            )
           ],
         ),
       ),
