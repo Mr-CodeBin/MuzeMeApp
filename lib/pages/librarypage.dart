9e264ec04993eda3f164c0ff951e1656508498a0
Nikhil Singh <nikhilsingh12032@gmail.com>
2023-04-07 13:26:39 +0530

    minor changes in lib page

--- lib/pages/librarypage.dart
+++ lib/pages/librarypage.dart
@@ -147,6 +147,7 @@ class _libPageState extends State<libPage> {
                         child: Image.asset(
                           "icons/shareIcon.png",
                           // fit: BoxFit.fill,
+                          //fixed height
                           height: 20,
                           width: 20,
                         ),
